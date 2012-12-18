7fc3d32736499e09a01b3bd84b304eae614f6aa6
Thomas Eriksson <thomas.eriksson@gmail.com>
2012-12-18 22:16:39 +0100

    fix for Q tail at 8 point

--- makemonovariant.sh
+++ makemonovariant.sh
@@ -4,8 +4,19 @@ COPYRIGHT="
 EB-Garamond 12 Mono was converted from EB-Garamond by the monospaci.py script by Thomas Eriksson (https://github.com/arnognulf/monospaci.py)
 
 EB-Garamond was created by Georg Duffner with FontForge 2.0 (http://fontforge.sf.net)" 
+SCALE=1.5
+python monospaci.py/monospaci.py -psname "EBGaramond12Mono" -fullname "EBGaramond12Mono" -copyright "$COPYRIGHT" -nameslist monospaci.py/NamesList.txt SFD/EBGaramond12-Regular.sfdir
+
+python monospaci.py/monospaci.py -leadingscale $SCALE -psname "EBGaramond12MonoHalfLead" -fullname "EBGaramond12MonoHalfLead" -copyright "$COPYRIGHT" -nameslist monospaci.py/NamesList.txt SFD/EBGaramond12-Regular.sfdir
+
+COPYRIGHT="
+EB-Garamond 8 Mono was converted from EB-Garamond by the monospaci.py script by Thomas Eriksson (https://github.com/arnognulf/monospaci.py)
+
+EB-Garamond was created by Georg Duffner with FontForge 2.0 (http://fontforge.sf.net)" 
  
-python monospaci.py/monospaci.py -psname "EB-Garamond12Mono" -fullname "EB-Garamond12Mono" -copyright "$COPYRIGHT" -nameslist monospaci.py/NamesList.txt build/EBGaramond12-Regular.otf
+python monospaci.py/monospaci.py -psname "EBGaramond8Mono" -fullname "EBGaramond8Mono" -copyright "$COPYRIGHT" -noscalechar Q -nameslist monospaci.py/NamesList.txt SFD/EBGaramond08-Mono.sfdir
+
+python monospaci.py/monospaci.py  -leadingscale $SCALE -psname "EBGaramond8MonoHalfLead" -fullname "EBGaramond8MonoHalfLead" -noscalechar Q -copyright "$COPYRIGHT" -nameslist monospaci.py/NamesList.txt SFD/EBGaramond08-Mono.sfdir
 
 rm *Output.ttf
 fontlint *.ttf
